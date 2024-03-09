fa803cf2cf8b07d489f7d0c17c695b95e786cf28
Craig Sacco <craig.sacco@gmail.com>
2024-03-09 18:52:40 +1100

    added test runner to the project, move code from example to unit tests

--- src/Example/src/main.cpp
+++ src/Example/src/main.cpp
@@ -23,30 +23,6 @@ void testTaskQueue()
     printf("Stopped\n");
 }
 
-void testSemaphore()
-{
-    printf("Testing the semaphore\n\n");
-
-    Gadgets::Core::Semaphore semaphore;
-
-    printf("Acquire: %d\n", semaphore.Acquire());
-    std::thread thread1([&semaphore] {std::this_thread::sleep_for(std::chrono::seconds(1)); semaphore.Release(); });
-    printf("Waiting: %d\n", semaphore.Wait(std::chrono::seconds(2)));
-    thread1.join();
-
-    printf("Release without acquire: %d\n", semaphore.Release());
-
-    printf("Acquire2: %d\n", semaphore.Acquire());
-    std::thread thread2([&semaphore] {std::this_thread::sleep_for(std::chrono::seconds(2)); semaphore.Release(); });
-    printf("Waiting2: %d\n", semaphore.Wait(std::chrono::seconds(1)));
-    printf("Overlap acquire: %d\n", semaphore.Acquire());
-    thread2.join();
-
-    printf("Acquire3: %d\n", semaphore.Acquire());
-    printf("Release3: %d\n", semaphore.Release());
-    printf("Waiting3: %d\n", semaphore.Wait(std::chrono::seconds(1)));
-}
-
 void testDeviceAndDriverInfrastructure()
 {
     printf("Testing the driver infrastructure\n\n");
@@ -88,10 +64,9 @@ void testDeviceAndDriverInfrastructure()
     printf("Stopped\n");
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     // testTaskQueue();
-    // testSemaphore();
     // testDeviceAndDriverInfrastructure();
 
     return 0;
---
+++ src/TestRunner/src/SemaphoreTests.cpp
@@ -0,0 +1,78 @@
+#include <Gadgets/Core/Semaphore.hpp>
+
+#include <gtest/gtest.h>
+
+#include <thread>
+
+using namespace ::testing;
+
+class SemaphoreTests : public Test
+{
+public:
+	SemaphoreTests()
+		: m_semaphore()
+		, m_thread()
+		, m_releaseResult(false)
+	{
+	}
+
+	void SetUp() override
+	{
+	}
+
+	void TearDown() override
+	{
+		if (m_thread.joinable())
+		{
+			m_thread.join();
+		}
+	}
+
+protected:
+	void CreateThreadForRelease(std::chrono::milliseconds delay_ms)
+	{
+		m_thread = std::thread([this, delay_ms] {
+			std::this_thread::sleep_for(delay_ms);
+			m_releaseResult = m_semaphore.Release();
+		});
+	}
+
+	Gadgets::Core::Semaphore m_semaphore;
+	std::thread m_thread;
+	std::atomic<bool> m_releaseResult;
+};
+
+TEST_F(SemaphoreTests, WaitBlocksUntilRelease)
+{
+	ASSERT_TRUE(m_semaphore.Acquire());
+	CreateThreadForRelease(std::chrono::milliseconds(500));
+	ASSERT_TRUE(m_semaphore.Wait(std::chrono::seconds(1)));
+	m_thread.join();
+	ASSERT_TRUE(m_releaseResult);
+}
+
+TEST_F(SemaphoreTests, WaitTimesOutBeforeRelease)
+{
+	ASSERT_TRUE(m_semaphore.Acquire());
+	CreateThreadForRelease(std::chrono::seconds(1));
+	ASSERT_FALSE(m_semaphore.Wait(std::chrono::milliseconds(500)));
+	m_thread.join();
+	ASSERT_TRUE(m_releaseResult);
+}
+
+TEST_F(SemaphoreTests, ReleaseWithoutAcquire)
+{
+	ASSERT_FALSE(m_semaphore.Release());
+}
+
+TEST_F(SemaphoreTests, WaitWithoutAcquire)
+{
+	ASSERT_TRUE(m_semaphore.Wait(std::chrono::seconds(1)));
+}
+
+TEST_F(SemaphoreTests, WaitAfterAcquireAndRelease)
+{
+	ASSERT_TRUE(m_semaphore.Acquire());
+	ASSERT_TRUE(m_semaphore.Release());
+	ASSERT_TRUE(m_semaphore.Wait(std::chrono::seconds(1)));
+}
